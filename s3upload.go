f2efcc9ed259c2c906ed91aafdca989a247c437d
Amged Rustom <amgadhs@codehill.com>
2014-09-11 16:28:54 +0400

    Don't upload files that already are in the S3 bucket.

--- s3upload.go
+++ s3upload.go
@@ -70,6 +70,12 @@ func uploadFile(path string) {
 
 	defer waiter.Done()
 
+	//if file exists do not upload again
+	if _, e := s3util.Open(cfg.Locations.Destination[locationPtr]+"/"+dest, nil); e == nil {
+		fmt.Printf("%s...Already exists.\n", dest)
+		return
+	}
+
 	r, err := os.Open(path)
 	if err != nil {
 		fmt.Printf("%s...%s", dest, err.Error())
